147cd5528af1ad67423f28096af31ef0ecc42a73
Tomohiro Kumagai <tomohiro@ez-style.jp>
2012-11-21 13:44:35 +0900

    Initial Commit

---
+++ EzSample_iOS_atomic/EzSampleViewController.m
@@ -0,0 +1,29 @@
+//
+//  EzSampleViewController.m
+//  EzSample_iOS_atomic
+//
+//  Created by 熊谷 友宏 on H.24/11/21.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import "EzSampleViewController.h"
+
+@interface EzSampleViewController ()
+
+@end
+
+@implementation EzSampleViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
